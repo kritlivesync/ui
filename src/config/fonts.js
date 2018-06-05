3e5379d34566c9da97810c5d614bc757d05a42f7
Chakkrit Tunyong <chakkrit@tunoz.com>
2018-06-05 09:15:57 +0700

    change lib font

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -1,26 +1,31 @@
 import normalize from '../helpers/normalizeText';
 export default {
-  size_10: normalize(10),
-  size_11: normalize(11),
-  size_12: normalize(12),
-  size_13: normalize(13),
-  size_14: normalize(14),
-  size_15: normalize(15),
-  size_16: normalize(16),
-  size_17: normalize(17),
-  size_18: normalize(18),
-  size_19: normalize(19),
-  size_20: normalize(20),
-  size_21: normalize(21),
-  size_22: normalize(22),
-  size_23: normalize(23),
-  size_24: normalize(24),
-  size_25: normalize(25),
-  size_26: normalize(26),
-  size_27: normalize(27),
-  size_28: normalize(28),
-  size_29: normalize(29),
-  size_30: normalize(30),
+  10: normalize(10),
+  11: normalize(11),
+  12: normalize(12),
+  13: normalize(13),
+  14: normalize(14),
+  15: normalize(15),
+  16: normalize(16),
+  17: normalize(17),
+  18: normalize(18),
+  19: normalize(19),
+  20: normalize(20),
+  21: normalize(21),
+  22: normalize(22),
+  23: normalize(23),
+  24: normalize(24),
+  25: normalize(25),
+  26: normalize(26),
+  27: normalize(27),
+  28: normalize(28),
+  29: normalize(29),
+  30: normalize(30),
+  mini: normalize(10),
+  small: normalize(13),
+  medium: normalize(15),
+  large: normalize(20),
+  xlarge: normalize(24),
   ios: {
     regular: {
       fontFamily: 'PingFangHK-Regular, sans-serif',
@@ -31,7 +36,6 @@ export default {
     extraBoldRegular: {
       fontFamily: 'Abadi MT Condensed Extra Bold Regular'
     },
-    /* We are only allowed to use these in iOS */
     SFMedium: {
       fontFamily: 'SFUIText-Medium'
     },
@@ -63,8 +67,6 @@ export default {
       fontWeight: 'light',
     },
     black: {
-      // note(brentvatne): sans-serif-black is only supported on Android 5+,
-      // we can detect that here and use it in that case at some point.
       fontFamily: 'sans-serif',
       fontWeight: 'bold',
     },
